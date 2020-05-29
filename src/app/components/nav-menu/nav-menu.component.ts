fa2dcde7f740b1c581a32cb9ff9e4370ef00e840
Javier Artero <javier.artero@gmail.com>
2020-05-29 08:31:31 +0200

    move nav-menu to custom angular component style

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostBinding,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavMenuComponent implements OnInit {
   @HostBinding('class') className = 'nav-menu';
